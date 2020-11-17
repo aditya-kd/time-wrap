e8ef95a7907f18488aae73598bc4ecc3999266fb
Aditya Sharma <aditya.sharma.cms17@gmail.com>
2020-11-17 11:34:22 +0530

    changed to a product list display type

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,15 @@
 import React from "react"
-import ContactCard from "./ContactCard"
+import Product from "./Product"
+import productsData from "./vsSchools"
 
-function App() {
-    return (
-        <div className="contacts">
-            <ContactCard 
-                contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
-            />
-            
-            <ContactCard 
-                contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
-            />
-            
-            <ContactCard
-                contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
-            />
-            
-            <ContactCard 
-                contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
-            />
-            
-        </div>
-    )
+function App(){
+    const productComponents = productsData.map(item=>
+        <Product key={item.id} product={item}/>)
+
+     return(
+            <div>
+                {productComponents}
+            </div>
+       )
 }
-
 export default App
---
+++ src/resources.md
@@ -0,0 +1,10 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findindex
+***this is the resource for different functions to be used***
+***this is helpful***
