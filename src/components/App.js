481bfd70faab425342587bb7a3da1c609fab6e90
Aditya Sharma <aditya.sharma.cms17@gmail.com>
2020-11-18 11:39:50 +0530

    added react-state

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,25 @@
-/*
+import React from "react"
+import "./component.css"
 
-Challenge:
-1. Convert all 3 components to be class-based
-2. Fix the small bug
-
-*/
-
-import React, { Component } from "react"
-
-
-// #1
-class main extends React.Component {
-    render( ){
-
-    return (
-        <div>
-            <Header username="Aditya Kumar Sharma"/>
-            <Greeting />
-        </div>    
-    )
+class App extends React.Component{
+    constructor()
+    {
+        super()//eslint-disable-next-line
+        {
+            this.state={
+                logged_in:true
+            }
+        }
     }
-}
-
-// #2
-class Header extends React.Component {
-    render (){
-    return (
-        <header>
-            <p>Welcome, {this.props.username}!</p>
-        </header>
-    )}
-}
-
-// #3
-class Greeting extends Component {
-   render(){
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay
-    
-    if (hours < 12) {
-        timeOfDay = "morning"
-    } else if (hours >= 12 && hours < 17) {
-        timeOfDay = "afternoon"
-    } else {
-        timeOfDay = "night"
+    render(){
+        let word
+        if(this.state.logged_in)
+        word=""
+        else word="not"
+        return(
+            <div className="genericDiv">
+                <h2>Hello, you are {word} welcome to enter.</h2>
+            </div>
+        )
     }
-    return (
-        <h1>Good {timeOfDay} to you, sir or madam!</h1>
-    )
-   }
-}
-export default main
+}export default App
